6d51ff76c03686d543c70d067e78b730dc3eb9d4
CEF388 <ceyda.fidan88@outlook.com>
2025-05-18 11:45:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,14 +10,13 @@ from fonksiyonlar import trendgetir
 
 diller=["TR","DE","IT","KR","FR","NL","DK"]
 
-guncelle=st.sidebar.button("Haberleri güncelle")
+guncelle=st.sidebar.button("Haberleri Güncelle")
 
 if guncelle:
-    for dil in diler:
+    for dil in diller:
         trendgetir(dil)
-    
 
-
+dilsecimi=st.multiselect("Ülke Seç",diller)
 ara=st.text_input("Haber İçinde Arama Yap")
 
 conn=sqlitecloud.connect('sqlitecloud://cgi896obnk.g5.sqlite.cloud:8860/chinook.sqlite?apikey=6gaa4nSztoaj9w4PCaVOIMgos4bNzOfLQyrHkeLsuho')
@@ -32,7 +31,7 @@ else:
 haberler=c.fetchall()
 
 if len(haberler)==0:
-    st.warning(f" {ara} sorgusu ile ilgili maalesef herhangi bir haber bulunamadı")
+    st.warning(f" {ara} sorgusu ile ilgili Herhangi Bir Haber bulunamadı")
 
 for i in range(0,len(haberler),3):
     col1,col2,col3=st.columns(3)
@@ -45,13 +44,14 @@ for i in range(0,len(haberler),3):
         if i+1 < len(haberler):
             st.image(haberler[i+1][3])
             st.write(haberler[i+1][1])
-            st.link_button("Habere Git", haberler[i+1][2])
+            st.link_button("Habere Git",haberler[i+1][2])
         else:
             pass
+
     with col3:
         if i+2 < len(haberler):
             st.image(haberler[i+2][3])
             st.write(haberler[i+2][1])
-            st.link_button("Habere Git", haberler[i+2][2])
+            st.link_button("Habere Git",haberler[i+2][2])
         else:
             pass
